6673bbcae4669df4e1c98c38455454a0acf8e67e
Charles Chamberlain <charlespipin@gmail.com>
2018-03-21 15:32:07 -0500

    Actually edit the tree; refactor the typing pipeline

--- ppx/ppx_relit.ml
+++ ppx/ppx_relit.ml
@@ -14,38 +14,41 @@ open Typedtree
 open Asttypes
 
 module To_current = Convert(OCaml_404)(OCaml_current)
+module From_current = Convert(OCaml_current)(OCaml_404)
 
 module TypedMap = TypedtreeMap.MakeMap(struct
+
     include TypedtreeMap.DefaultMapArgument
     let enter_expression expr =
-      (match expr.exp_desc with
-       | Texp_letmodule (
-           ident,
-           _loc,
-           { mod_desc = Tmod_ident (path, _loc'); mod_env; _ },
-           { exp_desc = Texp_apply (_raise_expr, [(_lbl, Some (
-                 { exp_desc = Texp_construct (_loc'', _desc, _err_info::{
-                       exp_desc = Texp_constant Const_string (source, _other_part ); _
-                     }::_); _ }
-               ))]); _ }
-         ) when Ident.name ident = "RelitInternalDefn" ->
-         prerr_endline source;
-         let mod_decl = Env.find_module path mod_env in
-         Printtyp.modtype
-           Format.err_formatter
-           mod_decl.md_type
-        | e -> ());
-      expr
+      match expr.exp_desc with
+      | Texp_letmodule (
+          ident,
+          loc,
+          ({ mod_desc = Tmod_ident (path, _loc); mod_env; _ } as module_expr),
+          ({ exp_desc = Texp_apply (_raise_expr, [(_lbl, Some (
+               { exp_desc = Texp_construct (_loc', _desc, _err_info::{
+                     exp_desc = Texp_constant Const_string (source, _other_part ); _
+                   }::_); _ }
+             ))]); _ } as top_expression)
+        ) when Ident.name ident = "RelitInternalDefn" ->
+        prerr_endline source;
+        let mod_type = (Env.find_module path mod_env).md_type in
+        Printtyp.modtype
+          Format.err_formatter
+          mod_type;
+        { expr with exp_desc = Texp_letmodule (
+              ident,
+              loc,
+              module_expr,
+              { top_expression with exp_desc = Texp_constant (Const_int 3)} ) }
+      | e -> expr
   end)
 
 let ppx_mapper _config _cookies =
   let structure_mapper _x structure =
 
-    (* enable this to run against any ast version if it can *)
-    let current_structure = To_current.copy_structure structure in
     (* useful definitions for the remaining part *)
-    let loc = (List.hd structure).pstr_loc in
-    let fname = loc.Location.loc_start.Lexing.pos_fname in
+    let fname = (List.hd structure).pstr_loc.Location.loc_start.Lexing.pos_fname in
     let without_extension = Filename.remove_extension fname in
 
     (* initialize the typechecking environment *)
@@ -55,16 +58,14 @@ let ppx_mapper _config _cookies =
     Env.set_unit_name module_name;
     let initial_env = Compmisc.initial_env () in
 
-    (* typecheck the structure *)
-    let typed = Typemod.type_implementation
-        fname without_extension module_name initial_env current_structure in
-
-    (* run our mapper against the typed tree *)
-    let mapped_typed = TypedMap.map_structure (fst typed) in
-
-    (* Printtyped.implementation Format.err_formatter mapped_typed; *)
-
+    (* map to current ast; typecheck; map typed tree; map back to known version *)
     structure
+    |> To_current.copy_structure
+    |> Typemod.type_implementation fname without_extension module_name initial_env
+    |> fst |> TypedMap.map_structure
+    |> fun x -> Printtyped.implementation Format.err_formatter x ; x
+    |> Untypeast.untype_structure
+    |> From_current.copy_structure
   in
   { default_mapper with structure = structure_mapper }
 
