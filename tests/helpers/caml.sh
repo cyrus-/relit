635606fec207c79f282ac8c4c89bc23009d35002
Charles Chamberlain <charlespipin@gmail.com>
2018-05-12 14:20:03 -0500

    Tests should pass for 4.04.2

--- tests/helpers/caml.sh
+++ tests/helpers/caml.sh
@@ -8,6 +8,7 @@ caml() {
     2>&1 \
     | sed 's/\/tmp\/cram.*\.ml/\{cram test file\}/g' \
     | grep -v 'Command line: ppx_relit' \
+    | grep -v 'File "_none_", line' \
     | grep -v 'Interface topdirs\.cmi occurs in several'
     # the above are slight hacks to remove random tmp names
   if [ -x "$tmp" ]; then "$tmp"; fi
